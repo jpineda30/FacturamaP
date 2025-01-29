03bc52ea6df7102c5a6e823568648b621368b96d
Jose Pineda Esquer <jose.pinedaesquer@gmail.com>
2025-01-29 15:17:16 -0600

    add authe

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post("/recibir-archivo", async (req, res) => {
     try {
       
       
-
+      var authFact = req.headers["authorization"]; 
       const response = await axios.get(
         "https://api.facturama.mx/"+req.body,
         
